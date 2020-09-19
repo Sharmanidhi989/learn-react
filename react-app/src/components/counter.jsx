c898f416ae748083f9dd0df54cbc77728514fad5
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-19 18:17:41 +0530

    use React.Fragment

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
 const Counter = () => {
-  return (<h1>Hey Counter</h1>);
-}
- 
+  // React.createElement('h1') i.e first param is type of element to be rendered
+  return (
+    <Fragment>
+      <h1>Hey Counter</h1>
+      <button>count ++</button>
+    </Fragment>
+  );
+};
+
 export default Counter;
