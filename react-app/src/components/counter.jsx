f21f60be3fe73b40c232263afff20f40be3fce85
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-20 00:14:00 +0530

    rendering list

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,26 +2,21 @@ import React, { Fragment, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const [imageUrl, setImageUrl] = useState("https://picsum.photos/200");
-
-  const styles = {
-    fontWeight: "bold",
-    fontSize: 40,
-  };
-
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
   // React.createElement('h1') i.e first param is type of element to be rendered
   return (
     <Fragment>
-      <img src={imageUrl}></img>
-      <span style={styles} className={newStyle(count)}>
-        {FormatCount(count)}
-      </span>
+      <span className={newStyle(count)}>{FormatCount(count)}</span>
       <button
         className="btn btn-secondary btn-sm"
         onClick={() => setCount(count + 1)}
       >
         count ++
       </button>
+      {renderTags(tags)}
+      {[].length === 0 && "given an empty array instead of tags"}
+      {/* when the value is not boolean then it will try to conclude whether the value is trusy or false and then return the value */}
+      {renderTags([])}
     </Fragment>
   );
 };
@@ -32,6 +27,20 @@ function newStyle(count) {
 
 function FormatCount(count) {
   return count === 0 ? "Zero" : count;
+}
+
+function renderTags(tags) {
+  if (tags.length === 0) {
+    return "No tags to display";
+  } else {
+    return (
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default Counter;
