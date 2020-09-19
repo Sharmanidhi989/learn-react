705653f0d325114ea3c655dd8413ad5bef0697ea
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-19 22:14:42 +0530

    embeding expressions

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,13 +1,37 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [imageUrl, setImageUrl] = useState("https://picsum.photos/200");
+
+  const styles = {
+    fontWeight: "bold",
+    fontSize: 40,
+  };
+
   // React.createElement('h1') i.e first param is type of element to be rendered
   return (
     <Fragment>
-      <h1>Hey Counter</h1>
-      <button>count ++</button>
+      <img src={imageUrl}></img>
+      <span style={styles} className={newStyle(count)}>
+        {FormatCount(count)}
+      </span>
+      <button
+        className="btn btn-secondary btn-sm"
+        onClick={() => setCount(count + 1)}
+      >
+        count ++
+      </button>
     </Fragment>
   );
 };
+
+function newStyle(count) {
+  return `m-2 badge ${count === 0 ? "badge-warning" : "badge-primary"}`;
+}
+
+function FormatCount(count) {
+  return count === 0 ? "Zero" : count;
+}
 
 export default Counter;
