2fd0ee72f78502c6e4a0b10fc7ec3715685aa836
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-21 23:52:00 +0530

    destructuring props

--- react-app/src/components/counters.jsx
+++ react-app/src/components/counters.jsx
@@ -2,19 +2,17 @@ import React, { Fragment } from "react";
 import Counter from "../components/counter";
 
 const Counters = (props) => {
+  const { handleDelete, handleReset, handleIncrement, counters } = props;
   return (
     <Fragment>
-      <button
-        onClick={props.handleReset}
-        className="btn btn-primary btn-sm m-2"
-      >
+      <button onClick={handleReset} className="btn btn-primary btn-sm m-2">
         Reset
       </button>
-      {props.counters.map((counter) => (
+      {counters.map((counter) => (
         <Counter
           key={counter.id}
-          onDelete={() => props.handleDelete(counter.id)}
-          onIncrement={() => props.handleIncrement(counter)}
+          onDelete={() => handleDelete(counter.id)}
+          onIncrement={() => handleIncrement(counter)}
           counter={counter}
         >
           <h4>I am a new counter: MY id #{counter.id}</h4>
