68c211354632dd9487a574acc565f2ea9be571b1
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-29 01:29:06 +0530

    nested routes

--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Users from "./users";
+import Posts from "./posts";
 
-const Dashboard = ({ match }) => {
+const Dashboard = ({ match: { url } }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/admin/users">Users</Link>
+          {/* bug admin/users */}
+        </li>
+        <li>
+          <Link to="/admin/posts">Posts</Link>
+        </li>
+      </ul>
+      <div className="container">
+        <Switch>
+          <Route path="/admin/users" component={Users} />
+          <Route path="/admin/posts" component={Posts} />
+        </Switch>
+      </div>
     </div>
   );
 };
