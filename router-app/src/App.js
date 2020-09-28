1f3ede882b18b5f0dfcb8f29f65fc10458e8f5dd
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-29 00:17:30 +0530

    redirect

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -26,7 +26,10 @@ class App extends Component {
             <Route path="/posts/:year?/:month?" component={Posts} />
             {/* when we pass params in routes by default they are required to make them optional add ? to them */}
             <Route path="/admin" component={Dashboard} />
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
             <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
           {/* <Route path="/" exact component={Home} /> exactly match the route */}
 
