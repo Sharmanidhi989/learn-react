22dca412820b37d4640a077225e035493dcfbb11
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-29 00:28:32 +0530

    programatic navigation

--- router-app/src/components/productDetails.jsx
+++ router-app/src/components/productDetails.jsx
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    // this.props.history.push("/products");
+    this.props.history.replace("/products"); // example: user login
   };
 
   render() {
@@ -14,5 +16,6 @@ class ProductDetails extends Component {
     );
   }
 }
-
+// push and replace is history param of router push will add a new element in the history to go back to
+// while replace will replace the current address so we don't have history
 export default ProductDetails;
