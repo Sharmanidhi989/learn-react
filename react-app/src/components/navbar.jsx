9719080ba89233888e1cc50880a107105ef896fa
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-29 02:11:41 +0530

    :lipstick: nav bar

--- react-app/src/components/navbar.jsx
+++ react-app/src/components/navbar.jsx
@@ -1,10 +1,22 @@
 import React, { Fragment } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <Fragment>
       <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">mOvIeSmAnIa</span>
+        <Link className="navbar-brand mb-0 h1" to="/">
+          mOvIeSmAnIa
+        </Link>
+        <NavLink className="nav-item nav-link" to="/movies">
+          Movies
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/customers">
+          Customers
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/rentals">
+          Rentals
+        </NavLink>
       </nav>
     </Fragment>
   );
