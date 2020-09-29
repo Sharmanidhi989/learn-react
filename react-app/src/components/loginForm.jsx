07aae2bb05f14d1b347c43ad01485f83b8fbf9b6
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-30 01:50:24 +0530

    :recycle: form

--- react-app/src/components/loginForm.jsx
+++ react-app/src/components/loginForm.jsx
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
 import Input from "./shared/input";
-class LoginForm extends Component {
-  state = { account: { username: "", password: "" }, errors: {} };
+import Form from "./shared/form";
+
+class LoginForm extends Form {
+  state = { data: { username: "", password: "" }, errors: {} };
   // null or undefined cannot be used as value of controlled element
 
   schema = {
@@ -10,48 +12,12 @@ class LoginForm extends Component {
     password: Joi.string().required(),
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-  };
-
-  validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false,
-    });
-    if (!result.error) return null;
-
-    const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProprerty(input);
-
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-
-  validateProprerty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const result = Joi.validate(obj, schema);
-
-    if (!result.error) return null;
-
-    return result.error.details[0].message;
+  doSubmit = () => {
+    console.log("Submitted");
   };
 
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <div className="card container m-5">
         <h1>Login</h1>
@@ -64,14 +30,14 @@ class LoginForm extends Component {
         >
           <Input
             name="username"
-            value={account.username}
+            value={data.username}
             label="Username"
             onChange={(e) => this.handleChange(e)}
             error={errors.username}
           />
           <Input
             name="password"
-            value={account.password}
+            value={data.password}
             label="Password"
             onChange={(e) => this.handleChange(e)}
             error={errors.password}
