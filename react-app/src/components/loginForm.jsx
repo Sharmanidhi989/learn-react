4b24f96519ef69f141ec4dcccaafb21e37895a4c
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-29 23:07:04 +0530

    validation

--- react-app/src/components/loginForm.jsx
+++ react-app/src/components/loginForm.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Input from "./shared/input";
 class LoginForm extends Component {
-  state = { account: { username: "", password: "" } };
+  state = { account: { username: "", password: "" }, errors: {} };
   // null or undefined cannot be used as value of controlled element
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
+  };
+
+  validate = () => {
+    return { username: "Username is required" };
   };
 
   handleChange = ({ currentTarget: input }) => {
