a3baabebf46a4d7e402b53afe90362c765b6122e
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-09-29 21:58:17 +0530

    handle onSubmit and learn Refs

--- react-app/src/components/loginForm.jsx
+++ react-app/src/components/loginForm.jsx
@@ -1,14 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 class LoginForm extends Component {
+  username = React.createRef();
+  // use of refs should be minimum
+
+  componentDidMount() {
+    this.username.current.focus();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let username = this.username.current.value;
+    console.log(username);
+  };
+
   render() {
     return (
       <div className="card container m-5">
         <h1>Login</h1>
         {/* form>(div.form-group>label+input.form-control)*2 */}
-        <form className="card-body">
+        <form
+          className="card-body"
+          onSubmit={(e) => {
+            this.handleSubmit(e);
+          }}
+        >
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
