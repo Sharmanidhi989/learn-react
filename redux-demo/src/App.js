fe7ec20b84909eddc48a77168d2df64ac244956a
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-10-22 00:27:44 +0530

    added nav bar

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import Header from "./components/shared/Header";
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/about" component={AboutPage}></Route>
-      <Route path="/" exact component={HomePage}></Route>
-    </Switch>
+    <Fragment>
+      <Header />
+       <Switch>
+          <Route path="/about" component={AboutPage}></Route>
+          <Route path="/" exact component={HomePage}></Route>
+        </Switch>
+    </Fragment>
   );
 }
 
---
+++ redux-demo/src/components/shared/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export const Header = () => {
+  const activeStyle = { color: "#800000" }
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
+      | <NavLink to="/about" activeStyle={activeStyle} >About</NavLink>
+    </nav>
+  );
+};
+
+export default Header;
