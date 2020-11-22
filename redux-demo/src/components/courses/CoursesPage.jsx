960178a199ec4cca4941be020c1c15f0c21e02fe
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-11-22 23:46:08 +0530

    added a simple form

--- redux-demo/src/components/courses/CoursesPage.jsx
+++ redux-demo/src/components/courses/CoursesPage.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 
 class CoursesPage extends Component {
-  state = {  }
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     course: { title: ""}
+  //   }
+
+  //   // this.handleChange = this.handleChange.bind(this); // binding in the constructor
+  //   // here the fuction is bound only once so wont be reallocated on every render.
+  // }
+
+  state = {
+    course: { title: ""}
+  }
+  // handleChange(event) + needs binding
+  handleChange = (event) => { 
+    // arrow function inherit the binding context of their enclosing scope
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({course});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title)
+  }
+
   render() { 
     return (
       <div>
-        <h2>Courses</h2>
+        <form onSubmit={this.handleSubmit} method="post">
+          <h2>Courses</h2>
+          <h3>Add Course</h3>
+          {/*  <input type="text" onChange={this.handleChange.bind(this)} value={this.state.course.title} name="title" id="title"/> */}
+          <input type="text" onChange={this.handleChange} value={this.state.course.title} name="title" id="title"/>
+          <input type="submit" value="save"/>
+        </form>
       </div>
     );
   }
