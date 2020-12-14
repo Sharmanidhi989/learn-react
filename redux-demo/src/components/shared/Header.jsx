0fac23afe02aa2b115ef0366e0e109b7db766a13
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-12-14 20:17:31 +0530

    collapsible navbar

--- redux-demo/src/components/shared/Header.jsx
+++ redux-demo/src/components/shared/Header.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState }from 'react';
+import { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
+  const [active, setActive] = useState(false);
   const activeStyle = { color: "#800000" }
+
+  const toggleNavBar = () => {
+    setActive(!active)
+  }
+
+  const navBarClass = active ? 'active' : ''
   return (
-    <nav>
-      <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
-      {'|'} <NavLink to="/about" activeStyle={activeStyle} >About</NavLink>
-      {'|'} <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>    
-    </nav>
+    <Fragment>
+      <span onClick={toggleNavBar}>&#9776; open</span>
+      <div className={"overlay " + navBarClass}>
+        <a className="closebtn" onClick={toggleNavBar}>&times;</a>
+        <div className="overlay-content">
+          <NavLink to="/" activeStyle={activeStyle} onClick={toggleNavBar} exact>Home</NavLink>
+          <NavLink to="/about" activeStyle={activeStyle} onClick={toggleNavBar} >About</NavLink>
+          <NavLink to="/courses" activeStyle={activeStyle} onClick={toggleNavBar} >Courses</NavLink> 
+        </div>
+      </div>
+    </Fragment>
+      
   );
 };
 
